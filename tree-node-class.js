9daeb0957be1f261fe5261a179a0720f90a75e0c
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2024-05-07 13:42:14 +0300

    Add files via upload

---
+++ tree-node-class.js
@@ -0,0 +1,21 @@
+
+
+
+class TreeNode {
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+const a = new TreeNode('a');
+const b = new TreeNode('b');
+const c = new TreeNode('c');
+const d = new TreeNode('d');
+const e = new TreeNode('e');
+const f = new TreeNode('f');
+
+
+
+module.exports = TreeNode;
